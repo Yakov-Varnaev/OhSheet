335edd97183c3b9d251e37c8148f5e0542106a8c
Yakov Varnaev <yakov.varnaev@mail.ru>
2022-02-16 02:47:37 +0300

    urls fix

--- oh_sheet/oh_sheet/urls.py
+++ oh_sheet/oh_sheet/urls.py
@@ -1,18 +1,19 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+import djoser.urls
 
 from .yasg import docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/api-auth/', include('rest_framework.urls')),
+    path('api/', include('users.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
 
 if settings.DEBUG:
     urlpatterns += [
         path('api/', include(docs_urls)),
-        path('api/', include('users.urls')),
-        path('auth/', include('djoser.urls')),
-        path('auth/', include('djoser.urls.jwt')),
     ]
