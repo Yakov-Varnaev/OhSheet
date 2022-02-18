54bae291da109c895402a0ce08432f2afd19896f
Yakov Varnaev <yakov.varnaev@mail.ru>
2022-02-18 18:28:59 +0300

    fixes

--- oh_sheet/characters/api/v1/serializers.py
+++ oh_sheet/characters/api/v1/serializers.py
@@ -1,4 +1,3 @@
-from django.forms import IntegerField
 from rest_framework import serializers
 
 from characters.models import CharacterClass, PlayableCharacter, Race
--- oh_sheet/characters/api/v1/views.py
+++ oh_sheet/characters/api/v1/views.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets
-from django.db.models import Count
 from characters.models import CharacterClass, PlayableCharacter, Race
 
 from .serializers import (CharacterClassSerializer, PlayableCharacterSerialzer,
