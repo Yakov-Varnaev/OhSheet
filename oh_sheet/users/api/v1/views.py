fd91a83b2dc88ff023c019476f039ec5dfce94be
Yakov Varnaev <yakov.varnaev@mail.ru>
2022-02-18 18:23:58 +0300

    code fix

--- oh_sheet/users/api/v1/views.py
+++ oh_sheet/users/api/v1/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.db.models import Count
 from rest_framework import mixins, viewsets
 
 from .serializers import UserProfileSerialier
@@ -13,12 +14,10 @@ class UserViewSet(mixins.ListModelMixin,
 
     def get_queryset(self):
         if self.action == 'list':
-            res = User.objects.raw(
-                '''
-                select users_user.id, first_name, last_name, bio, language, system, SUM(characters_playablecharacter.id) as character_count
-                from users_user
-                join characters_playablecharacter on users_user.id = characters_playablecharacter.owner_id;
-                '''
+            res = (
+                User
+                .objects
+                .annotate(characters_count=Count('characters'))
             )
             return res
         return User.objects.all()
