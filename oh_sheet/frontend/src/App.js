916d159a1cbfc82c5b680318b6402f2d7b2fb234
Yakov Varnaev <yakov.varnaev@mail.ru>
2022-02-19 14:33:35 +0300

    back to start

--- oh_sheet/frontend/src/App.js
+++ oh_sheet/frontend/src/App.js
@@ -1,14 +1,24 @@
-import "./App.css";
-import React from "react";
-import { Routes, BrowserRouter, Route } from "react-router-dom";
-import { Layout } from "./components/Layout/Layout";
-import { Login } from "./components/Auth/Login";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Layout>
-      <Login />
-    </Layout>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
