e1adcdaff1ddd0648711de56be0df338971244dc
Yakov-Varnaev <Yakov.varnaev@mail.ru>
2022-06-05 18:35:33 +0300

    user serializer test added

--- adventures/tests/test_serializers.py
+++ adventures/tests/test_serializers.py
@@ -1,5 +1,12 @@
 import pytest
 
+from users.serializers import UserSerializer
+
 @pytest.mark.django_db
 def test_adventure_serializer(user):
-    pass
+    serializer = UserSerializer(user)
+    data = serializer.data
+    fields = ['email', 'id', 'first_name', 'last_name']
+    for field in fields:
+        assert data.pop(field, False), f'UserSerializer must have `{field}` field.'
+    assert len(data) == 0, f'UserSerializer has extra fields {", ".join(data.keys())}'
--- users/serializers.py
+++ users/serializers.py
@@ -6,5 +6,5 @@ User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
+        model = User
         fields = ('id', 'email', 'first_name', 'last_name')
-
